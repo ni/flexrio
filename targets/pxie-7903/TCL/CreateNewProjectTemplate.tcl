af5e99acc512583831d03821e884845db3f0becb
rfmibuild <NIBuildFarmSupport@ni.com>
2025-05-29 18:23:47 -0500

    Merged PR 971945: [Refactor] Fix iwcompanion issues for Macallan targets.

--- targets/pxie-7903/TCL/CreateNewProjectTemplate.tcl
+++ targets/pxie-7903/TCL/CreateNewProjectTemplate.tcl
@@ -24,12 +24,4 @@ set_property steps.post_route_phys_opt_design.args.is_enabled "false" [get_runs 
 set_property steps.write_bitstream.tcl.post {$PPRDIR/../TCL/PostGenerateBitfile.tcl} [get_runs -filter !is_synthesis]
 set_property top TOP_ENTITY [current_fileset]
 
-# constraints.xdc is for use for both synthesis and implementation
-set_property used_in_synthesis true [get_files constraints.xdc]
-set_property used_in_implementation true [get_files constraints.xdc]
-
-# constraints_place.xdc is for use in implementation only
-set_property used_in_synthesis false [get_files constraints_place.xdc]
-set_property used_in_implementation true [get_files constraints_place.xdc]
-
 exit
