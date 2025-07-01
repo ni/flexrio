4e690cbe7abf35c71a4dafdcdc888cec194992df
Salvador Santoluctio <salvador.santolucito@ni.com>
2025-07-01 12:29:28 -0500

    fixing up code after re-merge from upstream.  Also adding LV FPGA bitfile script

--- targets/pxie-7903/TCL/PostGenerateBitfile.tcl
+++ targets/pxie-7903/TCL/PostGenerateBitfile.tcl
@@ -1,2 +1,8 @@
 #githubvisible=true
-exec "..\\..\\..\\lvfpgahdltools\\vivadoprojecttools\\createbitfile.bat"
+
+# Clear PYTHONPATH and PYTHONHOME
+set env(PYTHONPATH) ""
+set env(PYTHONHOME) ""
+
+# Execute the Python script
+exec python.bat ..\\..\\..\\..\\..\\lvfpgahdltools\\tools\\createlvbitfile.py
