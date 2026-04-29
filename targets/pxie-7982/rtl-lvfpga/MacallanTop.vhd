72328dd8a7c7380066bfcd07d02afa73ab81ed74
rfmibuild <NIBuildFarmSupport@ni.com>
2026-04-29 14:19:47 -0500

    updated to use new RIO DMA cores with generics

--- targets/pxie-7982/rtl-lvfpga/MacallanTop.vhd
+++ targets/pxie-7982/rtl-lvfpga/MacallanTop.vhd
@@ -774,8 +774,11 @@ begin  -- architecture struct
   --vhook_a bLvWindowRegPortIn  bRegPortIn
   --vhook_a bLvWindowRegPortOut bRegPortOut
   --vhook_g kHmbInUse true
+  --vhook_g kDmaFifoConfArrayGeneric kDmaFifoConfArray
   HostInterfacex: entity work.G3UsHostInterfaceIsoPort (struct)
-    generic map (kHmbInUse => true)  --boolean:=false
+    generic map (
+      kHmbInUse                => true,               --boolean:=false
+      kDmaFifoConfArrayGeneric => kDmaFifoConfArray)  --DmaChannelConfArray_t
     port map (
       PcieRefClk_p                             => PcieRefClk_p,                              --in  std_logic
       PcieRefClk_n                             => PcieRefClk_n,                              --in  std_logic
