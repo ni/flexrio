9cfc67bb5fb32f864467200c2952045e788e94dd
rfmibuild <NIBuildFarmSupport@ni.com>
2026-05-28 17:32:22 -0500

    Merged PR 1282080: Auto-upgrade ni.hw-flexrio.flexrio_baseboards to 26.5.0.51-ci-passed-main-d

--- targets/pxie-7986/rtl-lvfpga/AppletonTopTemplate.vhd
+++ targets/pxie-7986/rtl-lvfpga/AppletonTopTemplate.vhd
@@ -853,7 +853,6 @@ begin  -- architecture struct
   --vhook_nodgv {^Pcie[RT]x_[pn]}
 
   --vhook_e G3UspHostInterface HostInterfacex
-  --vhook_h kForceChannelEnable
   --vhook_a aGa                 aPxiGa
   --vhook_# Use BusClk for AxiClk and ViClk
   --vhook_a AxiClk              BusClk
@@ -872,11 +871,8 @@ begin  -- architecture struct
   --vhook_a bLvWindowRegPortIn  bRegPortIn
   --vhook_a bLvWindowRegPortOut bRegPortOut
   --vhook_g kHmbInUse true
-  --vhook_g kDmaFifoConfArrayGeneric kDmaFifoConfArray
   HostInterfacex: entity work.G3UspHostInterface (struct)
-    generic map (
-      kHmbInUse                => true,               --boolean:=false
-      kDmaFifoConfArrayGeneric => kDmaFifoConfArray)  --DmaChannelConfArray_t
+    generic map (kHmbInUse => true)  --boolean:=false
     port map (
       PcieRefClk_p                             => PcieRefClk_p,                              --in  std_logic
       PcieRefClk_n                             => PcieRefClk_n,                              --in  std_logic
